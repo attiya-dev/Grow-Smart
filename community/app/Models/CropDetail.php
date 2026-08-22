d8f1dbf290b4e314dd71a3fcd6afb111139f0fff
Abeeha604 <abeehaamanatali01@gmail.com>
2026-08-22 10:31:22 +0500

    Refactor CropDetail model properties and methods

--- community/app/Models/CropDetail.php
+++ community/app/Models/CropDetail.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Models;
-use App\Models\Crop;
+
 use Illuminate\Database\Eloquent\Model;
 
 class CropDetail extends Model
 {
-        protected $table = 'crop_details';
-        protected $fillable = [
+    protected $table = 'crop_details';
+
+    protected $fillable = [
         'crop_id',
         'crop_name',
         'introduction',
@@ -26,11 +27,10 @@ class CropDetail extends Model
         'crop_varieties',
         'nutritional_value',
         'importance_of_crop',
-        'modern_technologies',
     ];
 
     public function crop()
     {
-        return $this->belongsTo(Crop::class, 'crop_id');
+        return $this->belongsTo(Crop::class);
     }
 }
