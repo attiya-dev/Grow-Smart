76f0f4a9f5dd0c6238a3fb7fd6c40e54ad683434
Abeeha604 <abeehaamanatali01@gmail.com>
2026-08-22 10:26:01 +0500

    Refactor CropController methods for better organization

--- community/app/Http/Controllers/CropController.php
+++ community/app/Http/Controllers/CropController.php
@@ -1,65 +1,148 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Crop;
 use App\Models\CropDetail;
-use App\Models\PestManagement;
 use Illuminate\Http\Request;
 
 class CropController extends Controller
 {
+  
     public function grid()
-{
-    $summerCrops = Crop::where('season', 'summer')->get();
-    $winterCrops = Crop::where('season', 'winter')->get();
+    {
+        $summerCrops = Crop::where(
+            'season',
+            'summer'
+        )->get();
 
-    return view('grid', compact('summerCrops', 'winterCrops'));
-}
-   public function garden()
-{
-    $summerCrops = Crop::where('season', 'summer')->get();
-    $winterCrops = Crop::where('season', 'winter')->get();
+        $winterCrops = Crop::where(
+            'season',
+            'winter'
+        )->get();
 
-    return view('garden', compact('summerCrops', 'winterCrops'));
-}
- public function summer()
-{
-    $summerCrops = Crop::where('season', 'summer')->get();
+        return view(
+            'front.grid',
+            compact(
+                'summerCrops',
+                'winterCrops'
+            )
+        );
+    }
 
-    return view('summer', compact('summerCrops'));
-}
-public function winter()
-{
-    $winterCrops = Crop::where('season', 'winter')->get();
-    return view('winter', compact('winterCrops'));
-}
-public function grains()
-{
-    $crops = Crop::whereIn('name', ['Rice', 'Maize (Corn)','Sorghum', 'Pearl Millet', 'Mung', 'Soybean', 'Groundnut','Wheat', 'Barley','Rapeseed', 'Canola', 'Tobacco', 'Lucerne', 'Linseed', 'SunFlower', 'Isabgol'])->get();
-    return view('grains', compact('crops'));
-}
-public function fruit()
-{
-    $crops = Crop::whereIn('name', ['Banana', 'Mango', 'Lychee', 'Watermelon', 'Melon', 'Guava', 'Papaya', 'Peach','Orange','Kinnow','Lemon','GrapeFruit','Sweet Lime','Pomegranate','Apple','Date'])->get();
-    return view('fruit', compact('crops'));
-}
-public function vegetable()
-{
-    $crops = Crop::whereIn('name', ['Bottle Gourd', 'Okra', 'Tomato', 'Pumpkin', 'Bitter Gourd', 'Cucumber', 'EggPlant(Brinjal)','Chilli','Arum','Ginger','Turmeric','Fenugreek(Methi)','Potato','Spinach','Pea','Carrot','Onion','CauliFlower','Cabbage','Radish','Bean','Lettuce','Garlic','Beetroot','Turnip','Coriander','Broccoli','Mint','Fennel'])->get();
-    return view('vegetable', compact('crops'));
-}
- public function show(int $id)
-{
-     $crop = Crop::findOrFail($id);
+    public function garden()
+    {
+        $summerCrops = Crop::where(
+            'season',
+            'summer'
+        )->get();
 
-    $cropDetail = CropDetail::where('crop_id', $id)->first();
+        $winterCrops = Crop::where(
+            'season',
+            'winter'
+        )->get();
 
-   return view('crop-detail', compact('crop', 'cropDetail'));
-}
-public function pest(int $id)
-{
-    $crop = Crop::with('pestManagements')->findOrFail($id);
+        return view(
+            'front.garden',
+            compact(
+                'summerCrops',
+                'winterCrops'
+            )
+        );
+    }
 
-    return view('pest-detail', compact('crop'));
-}
+ 
+    public function summer()
+    {
+        $summerCrops = Crop::where(
+            'season',
+            'summer'
+        )->get();
+
+        return view(
+            'front.summer',
+            compact('summerCrops')
+        );
+    }
+
+    public function winter()
+    {
+        $winterCrops = Crop::where(
+            'season',
+            'winter'
+        )->get();
+
+        return view(
+            'front.winter',
+            compact('winterCrops')
+        );
+    }
+
+    public function grains()
+    {
+        $crops = Crop::where(
+            'category',
+            'grain'
+        )->get();
+
+        return view(
+            'front.grains',
+            compact('crops')
+        );
+    }
+
+    public function fruit()
+    {
+        $crops = Crop::where(
+            'category',
+            'fruit'
+        )->get();
+
+        return view(
+            'front.fruit',
+            compact('crops')
+        );
+    }
+
+    public function vegetable()
+    {
+        $crops = Crop::where(
+            'category',
+            'vegetable'
+        )->get();
+
+        return view(
+            'front.vegetable',
+            compact('crops')
+        );
+    }
+
+    public function show(int $id)
+    {
+        $crop = Crop::findOrFail($id);
+
+        $cropDetail = CropDetail::where(
+            'crop_id',
+            $id
+        )->first();
+
+        return view(
+            'front.crop-detail',
+            compact(
+                'crop',
+                'cropDetail'
+            )
+        );
+    }
+    public function pest(int $id)
+    {
+        $crop = Crop::with(
+            'pestManagements'
+        )->findOrFail($id);
+
+        return view(
+            'front.pest-detail',
+            compact('crop')
+        );
+    }
 }
