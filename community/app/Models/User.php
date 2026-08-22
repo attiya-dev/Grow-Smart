d39eed476ea4f065147ac555af240c5159fe9fd2
Abeeha604 <abeehaamanatali01@gmail.com>
2026-08-22 10:32:56 +0500

    Update User.php

--- community/app/Models/User.php
+++ community/app/Models/User.php
@@ -2,21 +2,41 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    use Notifiable;
+
     protected $table = 'users';
-   protected $fillable = [
+
+    protected $fillable = [
         'name',
         'email',
         'password',
+        'google_id',
+        'profile_photo',
         'is_admin',
-        'is_expert'
+        'is_expert',
+        'is_active',
     ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'is_admin' => 'boolean',
+            'is_expert' => 'boolean',
+            'is_active' => 'boolean',
+        ];
+    }
 
     public function questions()
     {
