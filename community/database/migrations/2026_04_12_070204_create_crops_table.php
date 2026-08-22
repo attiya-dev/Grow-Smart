358ff14107491ec78929dd4b58c84d7b517a2002
Abeeha604 <abeehaamanatali01@gmail.com>
2026-08-22 10:41:03 +0500

    Add 'category' field to crops migration
    Added 'category' enum field to crops table migration.

--- community/database/migrations/2026_04_12_070204_create_crops_table.php
+++ community/database/migrations/2026_04_12_070204_create_crops_table.php
@@ -6,24 +6,36 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('crops', function (Blueprint $table) {
+
             $table->id();
+
             $table->string('image');
-             $table->string('name');
-        $table->enum('season', ['summer', 'winter']);
-        $table->enum('type', ['indoor', 'outdoor'])->nullable();
+
+            $table->string('name');
+
+            $table->enum('season', [
+                'summer',
+                'winter'
+            ]);
+
+            $table->enum('type', [
+                'indoor',
+                'outdoor'
+            ])->nullable();
+
+            $table->enum('category', [
+                'fruit',
+                'vegetable',
+                'grain'
+            ]);
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('crops');
